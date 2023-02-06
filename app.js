5fbd1223454853e61f7ba1c8dacdc73ad8a411bb
therealdanmalmx <danmalmx@gmail.com>
2023-02-06 20:54:12 +0100

    install and use yargs. Add some functionality

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-const chalk = require('chalk');
-const getNotes = require('./notes');
+const chalk = require("chalk");
+const getNotes = require("./notes");
+const yargs = require("yargs"); //
 
-const command = process.argv[2];
+yargs.version("1.1.0");
 
-if (command === 'add') {
-    console.log("Adding note")
-} else if (command === 'delete') {
-    console.log("Deleting note")
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: (argv) => {
+    console.log("Title: ", argv.title);
+    console.log("Body: ", argv.body);
+  },
+});
+yargs.command({
+  command: "remove",
+  describe: "Remove a new note",
+  handler: () => {
+    console.log("Removing a new note");
+  },
+});
+yargs.command({
+  command: "read",
+  describe: "Read a new note",
+  handler: () => {
+    console.log("Reading a new note");
+  },
+});
+yargs.command({
+  command: "list",
+  describe: "List notes",
+  handler: () => {
+    console.log("Listing chosen notes");
+  },
+});
 
-}
+yargs.parse();
